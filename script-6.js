0966a7eb12635376f53fe959eb2e2a49a32e69bb
Lily Brazell <ljbrazell@gmail.com>
2016-08-12 14:47:28 -0500

    My website

---
+++ script-6.js
@@ -0,0 +1,38 @@
+/***************************
+This is JavaScript (JS), the programming language that powers the web (and this is a comment, which you can delete).
+
+To use this file, link it to your markup by placing a <script> in the <body> of your HTML file:
+
+  <body>
+    <script src="script.js"></script>
+
+replacing "script.js" with the name of this JS file.
+
+Learn more about JavaScript at
+
+https://developer.mozilla.org/en-US/Learn/JavaScript
+***************************/
+function myFunction() {
+  alert("Welcome to awesomeness");
+}
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
